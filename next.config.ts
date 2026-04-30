ff0e2428d5374c59b242a75ad305cb1693860272
Shubham Krishna <shubhamkrishnasound@gmail.com>
2026-04-30 17:41:07 +0530

    feat: configure static site export and add gh-pages deployment scripts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
+import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
+   basePath: '/dental_card',
+  assetPrefix: '/dental_card', 
   /* config options here */
+   output: 'export', // This creates a static 'out' folder
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
